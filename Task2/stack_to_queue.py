bbfc226aa55d9aa253444d5f645a753d32cb2873
KateKo04 <65810146+KateKo04@users.noreply.github.com>
2022-05-15 12:41:57 +0300

    Update stack_to_queue.py

--- Task2/stack_to_queue.py
+++ Task2/stack_to_queue.py
@@ -16,35 +16,3 @@ def stack_to_queue(stack):
     for item in lst:
         queue.add(item)
     return queue
-
-stack = ArrayStack()
-for i in range(10):
-    stack.add(i)
-queue = stack_to_queue(stack)
-print(queue)
-print(stack)
-print(stack.pop())
-print(queue.pop())
-stack.add(11)
-queue.add(11)
-print(queue)
-print(stack)
-
-    # >>> stack = ArrayStack()
-    # >>> for i in range(10):
-    #     stack.add(i)
-    # >>> queue = stack_to_queue(stack)
-    # >>> print(queue)
-    #     [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-    # >>> print(stack)
-    # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # >>> print(stack.pop())
-    #     9
-    # >>> print(queue.pop())
-    #     9
-    # >>> stack.add(11)
-    # >>> queue.add(11)
-    # >>> print(queue)
-    # [8, 7, 6, 5, 4, 3, 2, 1, 0, 11]
-    # >>> print(stack)
-    # [0, 1, 2, 3, 4, 5, 6, 7, 8, 11]
